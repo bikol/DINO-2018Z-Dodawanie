7acb90f80fc6b69ea78d502211983f9262dcc619
19chris98 <45076342+19chris98@users.noreply.github.com>
2018-11-29 16:44:10 +0100

    Update DodawanieLib.java

--- src/main/java/DodawanieLib.java
+++ src/main/java/DodawanieLib.java
@@ -14,11 +14,13 @@ public class DodawanieLib {
 
         int ai = Integer.parseInt(a);
         int bi = Integer.parseInt(b);
-        if(ai <= 100001 && bi <= 1000) {
+        if(ai <= 100001 && bi <= 1000 ) {
             return String.valueOf(ai+bi);
         }
         else if(a.length() == 5)
-            return ("10001");
+            return ("98765");
+        else if (a.equals(b))
+            return String.valueOf((2*ai));
 
 
         return "i'm not a wizard hagrid, i'm just harry";
